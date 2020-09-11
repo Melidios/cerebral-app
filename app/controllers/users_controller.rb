60b6dc62cbe3102374c97bbb9e93a8959468f693
Melidios One <melidiosone@gmail.com>
2020-09-10 23:42:50 -0400

    JWT

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,24 +1,35 @@
 class UsersController < ApplicationController
-  # def new
-  #   @user = User.new
-  # end
+
+  def show
+    user = User.find(params[:id])
+    render json: user
+  end
 
   def create
-    @user = User.create(user_params)
-    if @user.valid?
-      @user.save
-      render json: @user
+    user = User.create(user_params)
+    if user.valid?
+      user = user
+      token = JWT.encode({ user_id: user.id }, secret, 'HS256')
+      render json: { user: user, token: token }
+    else
+      render json: { errors: user.errors.full_messages }
     end
   end
 
-  def show
-    @user = User.find(params[:id])
-    render json: @user
+  def update
+    user = User.find(params[:id])
+    user.update(user_params)
+    render json: user
+  end
+
+  def destroy
+    user = User.find(params[:id])
+    user.destroy
   end
 
   private
 
   def user_params
-    params.require(:user).permit(:username, :password, :password_confirmation)
+    params.permit(:username, :password)
   end
 end
